f0fc24bb04e4abe97e2e5f449a796bf42b45abd2
saurabhrai <saurabhrai@gmail.com>
2016-01-18 16:05:49 +0530

    First commit

---
+++ src/factorial.java
@@ -0,0 +1,14 @@
+
+public class factorial
+{
+public static void main(String[] args) 
+{
+int a=5,b = 1;
+for (int i = 1; i <=a; i++) 
+{
+	b=b*i;//bdsljhbsdjlbvsjbs;kjbds
+		
+}
+System.out.println(b);
+}
+}
